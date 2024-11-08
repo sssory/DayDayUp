a9c4eb4ee8ae775267fb103bb4614641fd0647f2
sfj <15367308740@163.com>
2024-11-08 10:42:46 +0800

    #

--- DayDayUI/DayDayForm.Designer.cs
+++ DayDayUI/DayDayForm.Designer.cs
@@ -29,18 +29,12 @@
         private void InitializeComponent()
         {
             this.menu_head = new System.Windows.Forms.MenuStrip();
-            this.Menus = new System.Windows.Forms.ToolStripMenuItem();
-            this.Base_ApiOpen = new System.Windows.Forms.ToolStripMenuItem();
-            this.Base_ApiClose = new System.Windows.Forms.ToolStripMenuItem();
-            this.Base_ClearMessage = new System.Windows.Forms.ToolStripMenuItem();
-            this.Base_ReloadForm = new System.Windows.Forms.ToolStripMenuItem();
             this.status_foot = new System.Windows.Forms.StatusStrip();
             this.status_foot_service = new System.Windows.Forms.ToolStripStatusLabel();
             this.txt_msg = new System.Windows.Forms.TextBox();
             this.tab_body = new System.Windows.Forms.TabControl();
             this.tab_body_main = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
-            this.menu_head.SuspendLayout();
             this.status_foot.SuspendLayout();
             this.tab_body.SuspendLayout();
             this.tab_body_main.SuspendLayout();
@@ -48,48 +42,11 @@
             // 
             // menu_head
             // 
-            this.menu_head.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.Menus});
             this.menu_head.Location = new System.Drawing.Point(0, 0);
             this.menu_head.Name = "menu_head";
-            this.menu_head.Size = new System.Drawing.Size(1184, 25);
+            this.menu_head.Size = new System.Drawing.Size(1184, 24);
             this.menu_head.TabIndex = 0;
             this.menu_head.Text = "menuStrip1";
-            // 
-            // Menus
-            // 
-            this.Menus.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.Base_ApiOpen,
-            this.Base_ApiClose,
-            this.Base_ClearMessage,
-            this.Base_ReloadForm});
-            this.Menus.Name = "Menus";
-            this.Menus.Size = new System.Drawing.Size(59, 21);
-            this.Menus.Text = "Menus";
-            // 
-            // Base_ApiOpen
-            // 
-            this.Base_ApiOpen.Name = "Base_ApiOpen";
-            this.Base_ApiOpen.Size = new System.Drawing.Size(124, 22);
-            this.Base_ApiOpen.Text = "启动API";
-            // 
-            // Base_ApiClose
-            // 
-            this.Base_ApiClose.Name = "Base_ApiClose";
-            this.Base_ApiClose.Size = new System.Drawing.Size(124, 22);
-            this.Base_ApiClose.Text = "关闭API";
-            // 
-            // Base_ClearMessage
-            // 
-            this.Base_ClearMessage.Name = "Base_ClearMessage";
-            this.Base_ClearMessage.Size = new System.Drawing.Size(124, 22);
-            this.Base_ClearMessage.Text = "清除信息";
-            // 
-            // Base_ReloadForm
-            // 
-            this.Base_ReloadForm.Name = "Base_ReloadForm";
-            this.Base_ReloadForm.Size = new System.Drawing.Size(124, 22);
-            this.Base_ReloadForm.Text = "重新加载";
             // 
             // status_foot
             // 
@@ -125,10 +82,10 @@
             // 
             this.tab_body.Controls.Add(this.tab_body_main);
             this.tab_body.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tab_body.Location = new System.Drawing.Point(0, 25);
+            this.tab_body.Location = new System.Drawing.Point(0, 24);
             this.tab_body.Name = "tab_body";
             this.tab_body.SelectedIndex = 0;
-            this.tab_body.Size = new System.Drawing.Size(1184, 531);
+            this.tab_body.Size = new System.Drawing.Size(1184, 532);
             this.tab_body.TabIndex = 4;
             this.tab_body.MouseUp += new System.Windows.Forms.MouseEventHandler(this.tab_body_MouseUp);
             // 
@@ -138,7 +95,7 @@
             this.tab_body_main.Location = new System.Drawing.Point(4, 22);
             this.tab_body_main.Name = "tab_body_main";
             this.tab_body_main.Padding = new System.Windows.Forms.Padding(3);
-            this.tab_body_main.Size = new System.Drawing.Size(1176, 505);
+            this.tab_body_main.Size = new System.Drawing.Size(1176, 506);
             this.tab_body_main.TabIndex = 1;
             this.tab_body_main.Text = "welcome";
             this.tab_body_main.UseVisualStyleBackColor = true;
@@ -168,8 +125,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DayDayUp";
             this.Load += new System.EventHandler(this.DayDayForm_Load);
-            this.menu_head.ResumeLayout(false);
-            this.menu_head.PerformLayout();
             this.status_foot.ResumeLayout(false);
             this.status_foot.PerformLayout();
             this.tab_body.ResumeLayout(false);
@@ -186,14 +141,9 @@
         private System.Windows.Forms.StatusStrip status_foot;
         private System.Windows.Forms.TextBox txt_msg;
         private System.Windows.Forms.ToolStripStatusLabel status_foot_service;
-        private System.Windows.Forms.ToolStripMenuItem Menus;
         private System.Windows.Forms.TabControl tab_body;
         private System.Windows.Forms.TabPage tab_body_main;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ToolStripMenuItem Base_ClearMessage;
-        private System.Windows.Forms.ToolStripMenuItem Base_ReloadForm;
-        private System.Windows.Forms.ToolStripMenuItem Base_ApiOpen;
-        private System.Windows.Forms.ToolStripMenuItem Base_ApiClose;
     }
 }
 
